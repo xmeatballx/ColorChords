4ee6b9ee81ee2c60edbc2781051f09333fecec18
xmeatballx <erik.jensen5@pcc.edu>
2022-03-14 22:18:07 -0400

    refactored params class

--- src/js/params.js
+++ src/js/params.js
@@ -2,38 +2,55 @@ export class Params {
   constructor() {
     this.octave = 0;
     this.hold = true;
+    this.allOctaves = [];
+    this.visibleOctaves = [];
+    this.availableOctaves = 0;
   }
+
+  handleHold(e) {
+    const holdSwitchElement = e.currentTarget.children[0].children[0];
+    holdSwitchElement.classList.toggle("hold_switch-off");
+    this.hold = !this.hold;
+  }
+
   useOctave(e, octaves) {
-    const visibleOctaves = octaves.filter((octave) => {
+    this.allOctaves = octaves;
+    this.visibleOctaves = this.getVisibleOctaves(octaves);
+    this.availableOctaves = Math.floor((6 - visibleOctaves.length) / 2);
+
+    if (e.target.classList.contains("octave-up")) {
+      if (this.octave >= this.availableOctaves) return;
+      this.incrementOctave();
+    } else {
+      if (this.octave <= -this.availableOctaves) return;
+      this.decrementOctave();
+    }
+  }
+
+  getVisibleOctaves() {
+    return this.allOctaves.filter((octave) => {
       const displayStyleRule = window
         .getComputedStyle(octave)
         .getPropertyValue("display");
       return displayStyleRule == "block";
     });
-    const availableOctaves = Math.floor((6 - visibleOctaves.length) / 2);
+  }
 
-    if (e.target.classList.contains("octave-up")) {
-      if (this.octave >= availableOctaves) return;
-      this.incrementOctave();
-      visibleOctaves[0].style.display = "none";
-      octaves[
-        octaves.indexOf(visibleOctaves[visibleOctaves.length - 1]) + 1
-      ].style.display = "block";
-    } else {
-      if (this.octave <= -availableOctaves) return;
-      this.decrementOctave();
-      visibleOctaves[visibleOctaves.length - 1].style.display = "none";
-      octaves[octaves.indexOf(visibleOctaves[0]) - 1].style.display = "block";
-    }
-  }
-  handleHold(e) {
-    e.currentTarget.children[0].children[0].classList.toggle("hold_switch-off");
-    this.hold = !this.hold;
-  }
   incrementOctave() {
     this.octave++;
+    this.visibleOctaves[0].style.display = "none";
+    const highestVisibleOctave = this.visibleOctaves.pop();
+    const nextOctaveIndex = this.allOctaves.indexOf(highestVisibleOctave) + 1;
+    const nextOctave = this.allOctaves[nextOctaveIndex];
+    nextOctave.style.display = "block";
   }
+
   decrementOctave() {
     this.octave--;
+    const highestVisibleOctave = this.visibleOctaves.pop();
+    highestVisibleOctave.style.display = "none";
+    const precedingOctaveIndex = this.octaves.indexOf(visibleOctaves[0]) - 1;
+    const precedingOctave = this.allOctaves[precedingOctaveIndex];
+    precedingOctave.style.display = "block";
   }
 }
